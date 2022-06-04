e08cf87fa89908da4fd951cc47c78eaa24f4a052
Zephyr75 <antoine.familleroger@gmail.com>
2022-06-04 11:49:47 +0200

    Added test safety

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'dart:io';
 
 import 'package:cellulo_hub/custom_widgets/custom_scaffold.dart';
+import 'package:cellulo_hub/main/my_games.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -16,7 +17,7 @@ import 'package:cellulo_hub/main.dart';
 
 void main() {
 
-  testWidgets('My Games button goes to MyGames', (WidgetTester tester) async {
+  testWidgets('Shop button goes to Shop', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp(skipWelcome: true));
     await tester.pump();
 
@@ -26,9 +27,20 @@ void main() {
     expect(find.byIcon(MaterialCommunityIcons.lightbulb_on), findsOneWidget);
     expect(find.byIcon(Ionicons.ios_settings), findsOneWidget);
 
+    await tester.tap(find.text("Shop"));
+    await tester.pump();
+
+    expect(find.text("Physical"), findsOneWidget);
+  });
+
+  /*testWidgets('Add Game button goes to Shop', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp(skipWelcome: true));
+    await tester.pump();
     await tester.tap(find.byIcon(FontAwesome.gamepad));
+    await tester.pump();
+    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     //expect(find.byIcon(Icons.home), findsOneWidget);
-  });
+  });*/
 }
