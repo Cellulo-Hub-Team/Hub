0b0842dd196ca28617ec62bdefcfd46c2495af9c
Zephyr75 <antoine.familleroger@gmail.com>
2022-06-04 13:22:06 +0200

    Added better tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,12 @@
 
 import 'dart:io';
 
+import 'package:cellulo_hub/api/flutterfire_api.dart';
 import 'package:cellulo_hub/custom_widgets/custom_scaffold.dart';
+import 'package:cellulo_hub/firebase_options.dart';
 import 'package:cellulo_hub/main/my_games.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -17,30 +21,37 @@ import 'package:cellulo_hub/main.dart';
 
 void main() {
 
-  testWidgets('Shop button goes to Shop', (WidgetTester tester) async {
+  testWidgets('Main menu is correctly displayed', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp(skipWelcome: true));
     await tester.pump();
-
     expect(find.byIcon(FontAwesome.gamepad), findsOneWidget);
     expect(find.byIcon(Entypo.shop), findsOneWidget);
     expect(find.byIcon(MaterialCommunityIcons.medal), findsOneWidget);
     expect(find.byIcon(MaterialCommunityIcons.lightbulb_on), findsOneWidget);
     expect(find.byIcon(Ionicons.ios_settings), findsOneWidget);
-
-    await tester.tap(find.byIcon(FontAwesome.gamepad));
-    await tester.pump();
-
-    //expect(find.text("Physical"), findsOneWidget);
   });
 
-  /*testWidgets('Add Game button goes to Shop', (WidgetTester tester) async {
+  testWidgets('Shop button goes to Shop', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp(skipWelcome: true));
+    await tester.pump();
+    await tester.tap(find.byIcon(Entypo.shop));
+    await tester.pumpAndSettle();
+    expect(find.text("Shop"), findsOneWidget);
+  });
+
+  testWidgets('My Games button goes to My Games', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp(skipWelcome: true));
     await tester.pump();
     await tester.tap(find.byIcon(FontAwesome.gamepad));
-    await tester.pump();
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
+    expect(find.text("My games"), findsOneWidget);
+  });
 
-    //expect(find.byIcon(Icons.home), findsOneWidget);
-  });*/
+  testWidgets('Achievements button goes to Achievements', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp(skipWelcome: true));
+    await tester.pump();
+    await tester.tap(find.byIcon(MaterialCommunityIcons.medal));
+    await tester.pumpAndSettle();
+    expect(find.text("Achievements"), findsOneWidget);
+  });
 }
